303d61cf9f2633e8f2c68a6b99fb50beb633ee65
Mara Behles Dean <maraebehles@gmail.com>
2016-12-01 13:15:19 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Mara Behles Dean</h1>
+        <h4>Full-Stack Web Developer</h4>
+      </div>
+    )
+  }
+}
+
+// const App = () => <h1>Hello stateless</h1>
+
+export default App
